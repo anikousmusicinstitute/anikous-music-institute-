ce91d1093a0b7767ac42a6d162b61767802fece0
anikousmusicinstitute <anikousmusicinstitute@gmail.com>
2026-08-03 02:02:06 +0530

    script.js

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-function joinVideo(){
-    alert("Video Call Coming...");
-}
-
-function connectMIDI(){
-    alert("MIDI Connect Starting...");
-}
-function joinVideo(){
-    document.getElementById("video").style.display="block";
-}
+let audioContext;
 let midiAccess;
+let jitsiApi;
 
+
+// Jitsi Video Call
+function joinVideo(){
+
+    const domain = "meet.jit.si";
+
+    const options = {
+        roomName: "PianoLiveClassRoom",
+        width: "100%",
+        height: 600,
+        parentNode: document.querySelector("#jitsi-container"),
+        userInfo:{
+            displayName:"Teacher"
+        }
+    };
+
+    jitsiApi = new JitsiMeetExternalAPI(domain, options);
+}
+
+
+
+// MIDI Connect
 async function connectMIDI(){
+
+    audioContext = new AudioContext();
 
     if(navigator.requestMIDIAccess){
 
@@ -19,105 +35,120 @@ async function connectMIDI(){
         alert("MIDI Connected 🎹");
 
         midiAccess.inputs.forEach(input=>{
-            input.onmidimessage = playNote;
+
+            input.onmidimessage = playPiano;
+
         });
 
     }else{
+
         alert("MIDI not supported");
+
     }
 }
 
 
-function playNote(message){
 
-    let note = message.data[1];
-
-    console.log("Note:", note);
-
-    let keys = document.querySelectorAll(".key");
-
-    if(note){
-        keys[note % 7].style.background="yellow";
-
-        setTimeout(()=>{
-            keys[note % 7].style.background="white";
-        },200);
-    }
-}
-function joinVideo(){
-
-const domain = "meet.jit.si";
-
-const options = {
-    roomName: "PianoLiveClassRoom",
-    width: "100%",
-    height: 600,
-    parentNode: document.querySelector("#jitsi-container"),
-    userInfo:{
-        displayName:"Student"
-    }
-};
-
-const api = new JitsiMeetExternalAPI(domain, options);
-
-}
-let audioContext;
-let midiAccess;
-
-async function connectMIDI(){
-
-    audioContext = new AudioContext();
-
-    midiAccess = await navigator.requestMIDIAccess();
-
-    midiAccess.inputs.forEach((input)=>{
-        input.onmidimessage = playPiano;
-    });
-
-    alert("MIDI Connected 🎹");
-}
-
-
+// MIDI Key Press
 function playPiano(event){
 
     let note = event.data[1];
     let velocity = event.data[2];
+
 
     if(velocity > 0){
 
         document.getElementById("note").innerHTML =
         "Playing Note : " + note;
 
+
         playSound(note);
+
+
+        // Send to student
+        if(typeof socket !== "undefined"){
+
+            socket.emit("midiNote",{
+                note: note
+            });
+
+        }
+
+
+        lightKey(note);
+
     }
+
 }
 
 
+
+// Piano Sound
 function playSound(note){
 
     let oscillator = audioContext.createOscillator();
     let gain = audioContext.createGain();
 
+
     oscillator.frequency.value =
     440 * Math.pow(2,(note-69)/12);
+
 
     oscillator.connect(gain);
     gain.connect(audioContext.destination);
 
+
     gain.gain.value = 0.3;
+
 
     oscillator.start();
 
+
     setTimeout(()=>{
+
         oscillator.stop();
+
     },500);
+
 }
-socket.emit("midiNote",{
-    note: note
-});
-socket.on("studentNote",(data)=>{
 
-    document.getElementById("note").innerHTML =
-    "Teacher Playing: "+data.note;
 
-});
+
+// Light Piano Key
+function lightKey(note){
+
+    let keys = document.querySelectorAll(".key");
+
+    let key = keys[note % 7];
+
+
+    if(key){
+
+        key.style.background="yellow";
+
+
+        setTimeout(()=>{
+
+            key.style.background="white";
+
+        },200);
+
+    }
+
+}
+
+
+
+// Receive Student Side
+if(typeof socket !== "undefined"){
+
+    socket.on("studentNote",(data)=>{
+
+        document.getElementById("note").innerHTML =
+        "Teacher Playing: " + data.note;
+
+        lightKey(data.note);
+
+    });
+
+}
