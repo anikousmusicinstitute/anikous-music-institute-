ee2704138a30e127e15567038dc04f1a36983e50
anikousmusicinstitute <anikousmusicinstitute@gmail.com>
2026-08-02 16:56:12 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("join-room", (room) => {
+    socket.join(room);
+    socket.to(room).emit("user-joined");
+  });
+
+  socket.on("signal", (data) => {
+    socket.to(data.room).emit("signal", data.signal);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+http.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
