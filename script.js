8614be1cee741c2842ee723300e65123707ed77d
anikousmusicinstitute <anikousmusicinstitute@gmail.com>
2026-08-03 03:48:15 +0530

    script.js

--- script.js
+++ script.js
@@ -1,113 +1,91 @@
 function createPiano(){
 
+    let piano = document.getElementById("piano");
 
-let piano=document.getElementById("piano");
-
-
-piano.innerHTML="";
+    piano.innerHTML = "";
 
 
-let octave=document.getElementById("octave").value;
+    let octave = document.getElementById("octave").value;
 
 
-
-let whiteNotes=[
-
-"C",
-"D",
-"E",
-"F",
-"G",
-"A",
-"B"
-
-];
+    let whiteKeys = [
+        {name:"C", note:60},
+        {name:"D", note:62},
+        {name:"E", note:64},
+        {name:"F", note:65},
+        {name:"G", note:67},
+        {name:"A", note:69},
+        {name:"B", note:71}
+    ];
 
 
 
-whiteNotes.forEach(function(note,index){
-
-
-let key=document.createElement("div");
-
-
-key.className="white";
-
-
-key.innerHTML=note+octave;
-
-
-piano.appendChild(key);
-
-
-});
+    let blackKeys = [
+        {name:"C#", left:42},
+        {name:"D#", left:102},
+        {name:"F#", left:222},
+        {name:"G#", left:282},
+        {name:"A#", left:342}
+    ];
 
 
 
+    // White keys
+
+    whiteKeys.forEach(function(item,index){
 
 
-let blackNotes=[
+        let key=document.createElement("div");
 
-{
-name:"C#",
-left:45
-},
+        key.className="white";
 
-{
-name:"D#",
-left:105
-},
+        key.innerHTML =
+        item.name + octave;
 
-{
-name:"F#",
-left:225
-},
 
-{
-name:"G#",
-left:285
-},
+        key.style.order=index;
 
-{
-name:"A#",
-left:345
-}
 
-];
+        piano.appendChild(key);
+
+
+    });
 
 
 
 
 
-blackNotes.forEach(function(item){
+    // Black keys
+
+    blackKeys.forEach(function(item){
 
 
-let key=document.createElement("div");
+        let key=document.createElement("div");
 
 
-key.className="black";
+        key.className="black";
 
 
-key.innerHTML=item.name;
+        key.innerHTML=item.name;
 
 
-key.style.left=item.left+"px";
+        key.style.left=item.left+"px";
 
 
-piano.appendChild(key);
+        piano.appendChild(key);
 
 
-});
-
+    });
 
 
 }
 
 
 
+// page load
 
 window.onload=function(){
 
-createPiano();
+    createPiano();
 
 };
