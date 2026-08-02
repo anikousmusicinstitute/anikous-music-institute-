060e895fafe0f4c2be18d904e66bec514df82caf
anikousmusicinstitute <anikousmusicinstitute@gmail.com>
2026-08-03 01:19:56 +0530

    script.js

--- script.js
+++ script.js
@@ -8,3 +8,39 @@ function connectMIDI(){
 function joinVideo(){
     document.getElementById("video").style.display="block";
 }
+let midiAccess;
+
+async function connectMIDI(){
+
+    if(navigator.requestMIDIAccess){
+
+        midiAccess = await navigator.requestMIDIAccess();
+
+        alert("MIDI Connected 🎹");
+
+        midiAccess.inputs.forEach(input=>{
+            input.onmidimessage = playNote;
+        });
+
+    }else{
+        alert("MIDI not supported");
+    }
+}
+
+
+function playNote(message){
+
+    let note = message.data[1];
+
+    console.log("Note:", note);
+
+    let keys = document.querySelectorAll(".key");
+
+    if(note){
+        keys[note % 7].style.background="yellow";
+
+        setTimeout(()=>{
+            keys[note % 7].style.background="white";
+        },200);
+    }
+}
