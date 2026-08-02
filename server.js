ee2f522fee4b8c9f7841a6403fc4c19279140813
anikousmusicinstitute <anikousmusicinstitute@gmail.com>
2026-08-03 02:09:45 +0530

    server.js

--- server.js
+++ server.js
@@ -6,62 +6,39 @@ const app = express();
 
 const server = http.createServer(app);
 
-const io = new Server(server,{
-    cors:{
-        origin:"*"
-    }
-});
-
-io.on("connection",(socket)=>{
-
-    console.log("User connected");
-
-    socket.on("midiNote",(data)=>{
-        socket.broadcast.emit("studentNote",data);
-    });
-
-});
-
-server.listen(3000,()=>{
-    console.log("Server running");
-});
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-
-const app = express();
-
-const server = http.createServer(app);
-
-const io = new Server(server,{
-    cors:{
-        origin:"*"
+const io = new Server(server, {
+    cors: {
+        origin: "*"
     }
 });
 
 
-io.on("connection",(socket)=>{
+io.on("connection", (socket) => {
 
     console.log("User connected:", socket.id);
 
 
-    socket.on("midiNote",(note)=>{
+    socket.on("midiNote", (data) => {
 
         socket.broadcast.emit(
             "studentNote",
-            note
+            data
         );
 
     });
 
 
-    socket.on("disconnect",()=>{
-        console.log("User left");
+    socket.on("disconnect", () => {
+
+        console.log("User left:", socket.id);
+
     });
 
 });
 
 
-server.listen(3000,()=>{
+server.listen(3000, () => {
+
     console.log("Server running on 3000");
+
 });
