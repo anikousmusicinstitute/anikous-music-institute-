69cb426a76d9e83e8bd5144672163040f6c9b3cb
anikousmusicinstitute <anikousmusicinstitute@gmail.com>
2026-08-02 18:26:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
 
-app.use(express.static("."));
+const { Server } = require("socket.io");
+const io = new Server(http);
+
+app.use(express.static(__dirname));
 
 io.on("connection", (socket) => {
-  console.log("User connected");
+  console.log("User connected:", socket.id);
 
   socket.on("join-room", (room) => {
     socket.join(room);
-    socket.to(room).emit("user-joined");
+    console.log(`${socket.id} joined room ${room}`);
+
+    socket.to(room).emit("user-joined", socket.id);
   });
 
-  socket.on("signal", (data) => {
-    socket.to(data.room).emit("signal", data.signal);
+  socket.on("signal", ({ room, signal }) => {
+    socket.to(room).emit("signal", {
+      sender: socket.id,
+      signal: signal
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
   });
 });
 
 const PORT = process.env.PORT || 3000;
 
 http.listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+  console.log(`Server running on port ${PORT}`);
 });
