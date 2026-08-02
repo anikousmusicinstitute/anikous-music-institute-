aee0b3ad50876de17d71159a9413e4cfd3b413d3
anikousmusicinstitute <anikousmusicinstitute@gmail.com>
2026-08-03 03:01:34 +0530

    server.js

--- server.js
+++ server.js
@@ -6,16 +6,21 @@ const app = express();
 
 const server = http.createServer(app);
 
+
 const io = new Server(server, {
+
     cors: {
         origin: "*"
     }
+
 });
+
 
 
 io.on("connection", (socket) => {
 
     console.log("User connected:", socket.id);
+
 
 
     socket.on("midiNote", (data) => {
@@ -28,32 +33,25 @@ io.on("connection", (socket) => {
     });
 
 
+
     socket.on("disconnect", () => {
 
         console.log("User left:", socket.id);
 
     });
 
-});
-
-
-server.listen(3000, () => {
-
-    console.log("Server running on 3000");
 
 });
-window.onload = function(){
 
-    let piano = document.getElementById("piano");
 
-    piano.innerHTML = `
-        <div class="white">C1</div>
-        <div class="white">D1</div>
-        <div class="white">E1</div>
-        <div class="white">F1</div>
-        <div class="white">G1</div>
-        <div class="white">A1</div>
-        <div class="white">B1</div>
-    `;
 
-};
+const PORT = process.env.PORT || 3000;
+
+
+server.listen(PORT, () => {
+
+    console.log(
+        "Server running on " + PORT
+    );
+
+});
