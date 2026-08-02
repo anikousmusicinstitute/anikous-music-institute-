4ce9927ba9ef7a10573c49521beb80c0edb7830d
anikousmusicinstitute <anikousmusicinstitute@gmail.com>
2026-08-03 01:56:05 +0530

    server.js

--- server.js
+++ server.js
@@ -25,3 +25,43 @@ io.on("connection",(socket)=>{
 server.listen(3000,()=>{
     console.log("Server running");
 });
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+
+const server = http.createServer(app);
+
+const io = new Server(server,{
+    cors:{
+        origin:"*"
+    }
+});
+
+
+io.on("connection",(socket)=>{
+
+    console.log("User connected:", socket.id);
+
+
+    socket.on("midiNote",(note)=>{
+
+        socket.broadcast.emit(
+            "studentNote",
+            note
+        );
+
+    });
+
+
+    socket.on("disconnect",()=>{
+        console.log("User left");
+    });
+
+});
+
+
+server.listen(3000,()=>{
+    console.log("Server running on 3000");
+});
