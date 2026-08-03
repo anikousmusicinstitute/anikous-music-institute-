1a3a507a416cc17303c93d68a94a7980d7107f2c
anikousmusicinstitute <anikousmusicinstitute@gmail.com>
2026-08-03 08:31:26 +0530

    chords.js

--- chords.js
+++ chords.js
@@ -1,27 +1,24 @@
 const CHORDS = {
 
 "0,4,7":"Major",
-
 "0,3,7":"Minor",
-
 "0,3,6":"Diminished",
-
 "0,4,8":"Augmented",
-
 "0,5,7":"Sus4",
-
 "0,2,7":"Sus2",
 
 "0,4,7,10":"7",
-
 "0,4,7,11":"Maj7",
-
 "0,3,7,10":"m7",
 
+"0,3,6,9":"Dim7",
+"0,3,6,10":"m7b5",
+
 "0,4,7,10,2":"9",
+"0,3,7,10,2":"m9",
+"0,4,7,11,2":"Maj9",
 
 "0,4,7,10,2,5":"11",
-
 "0,4,7,10,2,5,9":"13"
 
 };
@@ -29,9 +26,11 @@ const CHORDS = {
 
 
 const NOTE_NAMES = [
+
 "C","C#","D","D#",
 "E","F","F#","G",
 "G#","A","A#","B"
+
 ];
 
 
@@ -47,7 +46,7 @@ return "-";
 
 
 
-let pitch = notes.map(n=>n % 12);
+let pitch = notes.map(n=>n%12);
 
 
 
@@ -63,12 +62,12 @@ return (n-root+12)%12;
 
 
 
-let type = CHORDS[intervals.join(",")];
+let chordType = CHORDS[intervals.join(",")];
 
 
-if(type){
+if(chordType){
 
-return NOTE_NAMES[root]+" "+type;
+return NOTE_NAMES[root]+" "+chordType;
 
 }
 
