f6620faeaa5b9a8392cfc4a6dd5709f16e677af5
Anikous Music Institute <anikousmusicinstitute@gmail.com>
2026-08-07 17:50:46 +0530

    firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,11 @@
-// Firebase configuration with your project details
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCxZ_pqLanqJgcQrVmFsFouQ17fmaYPy30",
   authDomain: "anikous-music-institute.firebaseapp.com",
@@ -9,6 +16,9 @@ const firebaseConfig = {
   measurementId: "G-C1DHT2QCCZ"
 };
 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 // Initialize Firebase (Compat version matching index.html)
 firebase.initializeApp(firebaseConfig);
 
